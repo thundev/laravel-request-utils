717d43e7cc6ff84625b4f6712c6c852d7b105c98
Vladimir <ordenko.v@gmail.com>
2021-09-23 15:00:18 +0300

    refactor_errors -  add tests for all errors methods

--- test/Errors.test.ts
+++ test/Errors.test.ts
@@ -24,3 +24,45 @@ test('this record errors', () => {
         expect(errors.get(key)).toBe(errorMessages[key][0]);
     });
 });
+test('this have any error', () => {
+    const errors = new Errors();
+
+    errors.record({
+        domain: ['The domain format is invalid.'],
+        thanks_page: ['The thanks page format is invalid.'],
+        webhook: ['The webhook format is invalid.'],
+    });
+
+    expect(errors.any()).toBeTruthy();
+});
+
+test('this get all errors', () => {
+    const errorMessages: { [key: string]: string[] } = {
+        domain: ['The domain format is invalid.'],
+        thanks_page: ['The thanks page format is invalid.'],
+        webhook: ['The webhook format is invalid.'],
+    };
+    const errors = new Errors();
+
+    errors.record(errorMessages);
+    expect(errors.getAll()).toEqual(Object.values(errorMessages).map((value) => value[0]));
+});
+
+test('this clear one or all error fields', () => {
+    const errorMessages: { [key: string]: string[] } = {
+        domain: ['The domain format is invalid.'],
+        thanks_page: ['The thanks page format is invalid.'],
+        webhook: ['The webhook format is invalid.'],
+    };
+    const errors = new Errors();
+
+    errors.record(errorMessages);
+    expect(errors.getAll()).toEqual(Object.values(errorMessages).map((value) => value[0]));
+
+    errors.clear('domain');
+    expect(errors.has('thanks_page')).toBeTruthy();
+    expect(errors.has('webhook')).toBeTruthy();
+
+    errors.clear();
+    expect(errors.any()).toBe(false);
+});
